07a9de1049601ac42714923606e3a39d839b2450
Yunsik Park <space214@gmail.com>
2017-06-16 00:47:54 +0900

    logout view 구현

--- django_app/member/urls.py
+++ django_app/member/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import url
 
 from . import views
 
-app_name='member'
+app_name = 'member'
 urlpatterns = [
     url(r'^login/$', views.login, name='login'),
+    url(r'^logout/$', views.logout, name='logout'),
 ]
