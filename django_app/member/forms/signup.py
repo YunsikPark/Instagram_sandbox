f2268447962b95f0c1af20da3c91942d0bef79ee
Yunsik Park <space214@gmail.com>
2017-06-18 20:03:40 +0900

    SignupForm사용

--- django_app/member/forms/signup.py
+++ django_app/member/forms/signup.py
@@ -16,9 +16,27 @@ class SignupForm(forms.Form):
     )
 
     def clean_username(self):
-        username = self.cleaned_data['username']
-        if User.objects.filter(username=username).exists():
+        username = self.cleaned_data.get('username')
+        if username and User.objects.filter(username=username).exists():
             raise forms.ValidationError(
                 'Username already exist'
             )
         return username
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError(
+                'Password mismatch',
+            )
+        return password2
+
+    def create_user(self):
+        username = self.cleaned_data['username']
+        password = self.cleaned_data['password2']
+        user = User.objects.create_user(
+            username=username,
+            password=password
+        )
+        return user
